1fcc998242351b1d43b100517367bbbcb416a3bf
Sizhe Fu <50268764+BarclayFu@users.noreply.github.com>
2025-12-07 00:14:10 +0800

    feat:更新Day6 Q2

--- Day6/Q2.java
+++ Day6/Q2.java
@@ -15,9 +15,11 @@ public class Q2 {
       String line = br.readLine();
       // 根据最后一行的字符确定数字开头
       List<Integer> starts = new ArrayList<>();
+      List<Character> chars = new ArrayList<>();
       for (int i = 0; i < line.length(); i++) {
         if (line.charAt(i) != ' ') {
           starts.add(i);
+          chars.add(line.charAt(i));
         }
       }
 
@@ -30,11 +32,10 @@ public class Q2 {
       }
 
       // 切出每个 cell，包含前后空格
-      int rowCount = records.size();
       int colCount = segments.size();
-      String[][] cells = new String[rowCount][colCount];
+      String[][] cells = new String[4][colCount];
 
-      for (int r = 0; r < rowCount; r++) {
+      for (int r = 0; r < 4; r++) {
         String l = records.get(r);
         for (int c = 0; c < colCount; c++) {
           int s = segments.get(c)[0];
@@ -42,10 +43,31 @@ public class Q2 {
           cells[r][c] = l.substring(s, e);
         }
       }
+      long res = 0;
       for (int i = 0; i < colCount; i++) {
-
+        // 一列数字
+        String first = cells[0][i];
+        long sum = 0L;
+        if(chars.get(i) == '*') sum = 1L;
+        // 每一个cell列，长度取第一个的长度就行
+        for(int j = first.length() - 1; j >= 0; j--) {
+          // 一列中的每一列cell
+          int cur = 0;
+          for(int k = 0; k < 4; k++){
+            if(cells[k][i].charAt(j) != ' '){
+              cur = cur * 10 + (cells[k][i].charAt(j) - '0');
+            }
+          }
+          if(cur == 0) continue;
+          if(chars.get(i) == '+'){
+            sum += cur;
+          }else{
+            sum *= cur;
+          }
+        }
+        res += sum;
       }
-
+      System.out.println(res);
     } catch (IOException e) {
       e.printStackTrace();
     }
